fd9246657cdd340b0ae4e9d0c283c0f819f6c4e2
Sandeep <singh.192@iitj.ac.in>
2023-12-21 23:41:35 +0530

    header updation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
-    Link,
-    } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from '../chatbot.png';
 
 export default function Header() {
